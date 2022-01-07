1d9af14c4d5f041a10b943289ea288b37be611ce
michal <michal1995.1995@interia.pl>
2022-01-07 12:17:49 +0100

    start with mvc design pattern

---
+++ lib/src/controller/main_controller.dart
@@ -0,0 +1,29 @@
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:textscan/src/model/Model.dart';
+
+class Controller extends ControllerMVC {
+  factory Controller() {
+    // if (_this == null) _this ??= Controller._();
+    return _this ??= Controller._();
+  }
+  static Controller? _this;
+
+  Controller._();
+
+  /// Allow for easy access to 'the Controller' throughout the application.
+  static Controller? get con => _this;
+
+  int get counter => Model.counter;
+  void incrementCounter() {
+    /// The Controller knows how to 'talk to' the Model. It knows the name, but Model does the work.
+    Model.incrementCounter();
+  }
+
+  void decrement() {
+    /// The Controller knows how to 'talk to' the Model. It knows the name, but Model does the work.
+    Model.decrementCounter();
+  }
+  // static int get counter => Model.counter;
+  // static void incrementCounter() => Model.incrementCounter();
+  // static void decrementCounter() => Model.decrementCounter();
+}
